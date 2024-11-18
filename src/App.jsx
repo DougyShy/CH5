fe92fceedabfb5a39dc6c322831372a4c3c69729
Doug Scheible <doug_scheible@yahoo.com>
2024-11-18 17:43:48 -0600

    chore: seperate posts

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+/*import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import { Blog } from './pages/Blog.jsx'
 import { Signup } from './pages/Signup.jsx'
-import { Login } from './pages/Login.jsx'
+import { Login } from './pages/Login.jsx'*/
+import PropTypes from 'prop-types'
 import { AuthContextProvider } from './contexts/AuthContext.jsx'
 
 const queryClient = new QueryClient()
 
-const router = createBrowserRouter([
+/*const router = createBrowserRouter([
   {
     path: '/',
     element: <Blog />,
@@ -20,14 +21,16 @@ const router = createBrowserRouter([
     path: '/login',
     element: <Login />,
   },
-])
+])*/
 
-export function App() {
+export function App({ children }) {
   return (
     <QueryClientProvider client={queryClient}>
-      <AuthContextProvider>
-        <RouterProvider router={router} />
-      </AuthContextProvider>
+      <AuthContextProvider>{children}</AuthContextProvider>
     </QueryClientProvider>
   )
+}
+
+App.propTypes = {
+  children: PropTypes.element.isRequired,
 }
